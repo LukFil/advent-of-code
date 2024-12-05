5149dbac0dc3e2c256641252bdcf6a9c768c9b4d
LukFil <lukas.p.filipcik@gmail.com>
2024-12-05 01:07:08 +0100

    feat: comitting previous code

---
+++ 2023/02_solution.ts
@@ -0,0 +1,58 @@
+import fs from "fs/promises";
+
+async function main() {
+  const rawData = await fs.readFile("./02_input.txt");
+  const data = rawData.toString();
+
+  const splitByBreak = data.split("\n");
+  const limit = [
+    { number: 12, color: "red" },
+    { number: 13, color: "green" },
+    { number: 14, color: "blue" },
+  ];
+  const colors = ["red", "green", "blue"];
+
+  const res = splitByBreak
+    .map((line) => {
+      if (line.length < 1) return;
+      const splitLine = line.split(":");
+      const gameId = splitLine[0].split(" ")[1];
+
+      const colors = splitLine[1].split(/[,+;]/).map((c) => {
+        const splitC = c.split(" ");
+
+        return { color: splitC[2], number: Number(splitC[1]) };
+      });
+
+      return { gameId, colors };
+    })
+    .map((map) => {
+      const res = colors.map((cl) => {
+        return map?.colors
+          .filter((p) => p.color === cl)
+          .sort((a, b) => (a.number > b.number ? -1 : 1))[0].number;
+      });
+      return res.map((p) => Number(p)).reduce((acc, curr) => acc * curr);
+      // const res = limit
+      //   .map((l) => {
+      //     const exceeds = map?.colors.filter(
+      //       (m) => m.color === l.color && m.number > l.number,
+      //     );
+      //     if (exceeds) return exceeds;
+      //     return undefined;
+      //   })
+      //   .flat();
+      // if (map === undefined || map.gameId === undefined) return;
+      // if (res.length === 0) return { gameId: map.gameId };
+      // return;
+    })
+    .filter((p) => !Number.isNaN(p))
+    .reduce((acc, curr) => acc + curr);
+  // .filter((p) => p !== undefined && p.gameId !== undefined)
+  // .map((e) => Number(e?.gameId))
+  // .reduce((acc, curr) => acc + curr);
+
+  console.log(res);
+}
+
+main();
--- 2023/index.ts
+++ 2023/index.ts
@@ -1,58 +1,93 @@
 import fs from "fs/promises";
 
-async function main() {
-  const rawData = await fs.readFile("./02_input.txt");
+function findAllIndexes(str: string, search: string) {
+  let indexes: Array<number> = [];
+
+  let i = 0;
+  while (i < str.length) {
+    const index = str.indexOf(search, i);
+    if (index === -1) return indexes;
+
+    i = i + index + search.length;
+    indexes.push(index);
+  }
+
+  return indexes;
+}
+
+function findNumbersInMatrix(
+  matrix: string[],
+  ids: [number, number][],
+): number[] {
+  const res = ids.map((coords) => {
+    // check for matrix edges
+    console.log(matrix[coords[0]][coords[1]]);
+  });
+
+  return [];
+}
+
+async function firstApproach() {
+  const rawData = await fs.readFile("./03_input.txt");
   const data = rawData.toString();
 
   const splitByBreak = data.split("\n");
-  const limit = [
-    { number: 12, color: "red" },
-    { number: 13, color: "green" },
-    { number: 14, color: "blue" },
-  ];
-  const colors = ["red", "green", "blue"];
+  let ids: [number, number][] = [];
 
-  const res = splitByBreak
-    .map((line) => {
-      if (line.length < 1) return;
-      const splitLine = line.split(":");
-      const gameId = splitLine[0].split(" ")[1];
+  splitByBreak.forEach((line, lineNumber) => {
+    const matches = line.match(/[^a-zA-Z\d.]/g);
+    const setMatches = new Set(matches);
+    Array.from(setMatches).forEach((match) => {
+      return findAllIndexes(line, match).forEach((i) =>
+        ids.push([lineNumber, i]),
+      );
+    });
+  });
 
-      const colors = splitLine[1].split(/[,+;]/).map((c) => {
-        const splitC = c.split(" ");
+  findNumbersInMatrix(splitByBreak, ids);
+}
 
-        return { color: splitC[2], number: Number(splitC[1]) };
-      });
+const checkOne = (ch:string) => {
+ if (!ch) return undefined
+ if (ch.match(/[^a-zA-Z\d.]/) === null) return false
 
-      return { gameId, colors };
-    })
-    .map((map) => {
-      const res = colors.map((cl) => {
-        return map?.colors
-          .filter((p) => p.color === cl)
-          .sort((a, b) => (a.number > b.number ? -1 : 1))[0].number;
-      });
-      return res.map((p) => Number(p)).reduce((acc, curr) => acc * curr);
-      // const res = limit
-      //   .map((l) => {
-      //     const exceeds = map?.colors.filter(
-      //       (m) => m.color === l.color && m.number > l.number,
-      //     );
-      //     if (exceeds) return exceeds;
-      //     return undefined;
-      //   })
-      //   .flat();
-      // if (map === undefined || map.gameId === undefined) return;
-      // if (res.length === 0) return { gameId: map.gameId };
-      // return;
-    })
-    .filter((p) => !Number.isNaN(p))
-    .reduce((acc, curr) => acc + curr);
-  // .filter((p) => p !== undefined && p.gameId !== undefined)
-  // .map((e) => Number(e?.gameId))
-  // .reduce((acc, curr) => acc + curr);
+ return true
 
-  console.log(res);
+}
+
+const checkCol = (matrix: string[], coords: [number, number], debug?: string) => {
+  if (!matrix[coords[0]][coords[1]]) return false;
+
+  const ch1 = matrix[coords[0]][coords[1] - 1];
+  const ch2 = matrix[coords[0]][coords[1]];
+  const ch3 = matrix[coords[0]][coords[1] + 1];
+
+ return [checkOne(ch1), checkOne(ch2), checkOne(ch3)]
+};
+
+async function main() {
+  const rawData = await fs.readFile("./03_input.txt");
+  const data = rawData.toString();
+
+  const splitByBreak = data.split("\n");
+
+  const res = splitByBreak.map((line, lineNumber) => {
+    const numbers = line.match(/\d{1,}/g);
+    // console.log(numbers);
+    const found = numbers?.map((num) => {
+      let innerFound: boolean[] = [];
+      const numIndex = line.indexOf(num);
+
+      const cols = checkCol(splitByBreak, [lineNumber, numIndex], line)
+      if (cols === false) throw new Error ('we re indexing nonexistent spot')
+      const isAttached  = cols.some(p => p === true)
+
+      innerFound.push(isAttached);
+      console.log(innerFound);
+    });
+  });
+
+  // console.log(ids);
 }
 
 main();
